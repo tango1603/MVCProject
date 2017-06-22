1058648dd6cdf06eb4a07445362ace0c992b37d8
Ilya Bashilov <tango1603@yandex.ru>
2017-06-22 23:28:04 +0300

    add color mark for sort

--- MVCProject/MVCProject/Scripts/AngularController/TableAddressController.js
+++ MVCProject/MVCProject/Scripts/AngularController/TableAddressController.js
@@ -1,10 +1,81 @@
 ﻿angular.module('MyApp')
     .controller('TableAddressController', function ($scope, AddressService) {
 
+        //цвет выделения
+        $scope.setColorId = function () {
+            $scope.colorId = { "color": "blue" }
+            $scope.colorCountry = { "color": "black" }
+            $scope.colorCity = { "color": "black" }
+            $scope.colorStereet = { "color": "black" }
+            $scope.colorBuildingNumber = { "color": "black" }
+            $scope.colorPostID = { "color": "black" }
+            $scope.colorDateTime = { "color": "black" }
+        }
+
+        $scope.setColorCountry = function () {
+            $scope.colorId = { "color": "black" }
+            $scope.colorCountry = { "color": "blue" }
+            $scope.colorCity = { "color": "black" }
+            $scope.colorStereet = { "color": "black" }
+            $scope.colorBuildingNumber = { "color": "black" }
+            $scope.colorPostID = { "color": "black" }
+            $scope.colorDateTime = { "color": "black" }
+        }
+
+        $scope.setColorCity = function () {
+            $scope.colorId = { "color": "black" }
+            $scope.colorCountry = { "color": "black" }
+            $scope.colorCity = { "color": "blue" }
+            $scope.colorStereet = { "color": "black" }
+            $scope.colorBuildingNumber = { "color": "black" }
+            $scope.colorPostID = { "color": "black" }
+            $scope.colorDateTime = { "color": "black" }
+        }
+
+        $scope.setColorStereet = function () {
+            $scope.colorId = { "color": "black" }
+            $scope.colorCountry = { "color": "black" }
+            $scope.colorCity = { "color": "black" }
+            $scope.colorStereet = { "color": "blue" }
+            $scope.colorBuildingNumber = { "color": "black" }
+            $scope.colorPostID = { "color": "black" }
+            $scope.colorDateTime = { "color": "black" }
+        }
+
+        $scope.setColorBuildingNumber = function () {
+            $scope.colorId = { "color": "black" }
+            $scope.colorCountry = { "color": "black" }
+            $scope.colorCity = { "color": "black" }
+            $scope.colorStereet = { "color": "black" }
+            $scope.colorBuildingNumber = { "color": "blue" }
+            $scope.colorPostID = { "color": "black" }
+            $scope.colorDateTime = { "color": "black" }
+        }
+
+        $scope.setColorPostID = function () {
+            $scope.colorId = { "color": "black" }
+            $scope.colorCountry = { "color": "black" }
+            $scope.colorCity = { "color": "black" }
+            $scope.colorStereet = { "color": "black" }
+            $scope.colorBuildingNumber = { "color": "black" }
+            $scope.colorPostID = { "color": "blue" }
+            $scope.colorDateTime = { "color": "black" }
+        }
+
+        $scope.setColorDateTime = function () {
+            $scope.colorId = { "color": "black" }
+            $scope.colorCountry = { "color": "black" }
+            $scope.colorCity = { "color": "black" }
+            $scope.colorStereet = { "color": "black" }
+            $scope.colorBuildingNumber = { "color": "black" }
+            $scope.colorPostID = { "color": "black" }
+            $scope.colorDateTime = { "color": "blue" }
+        }
+
         //Фильтр 
         $scope.sortType = 'Id'; // значение сортировки по умолчанию
+        $scope.colorId = { "color": "blue" } // значение цвета по умолчанию
         $scope.sortReverse = false;  // обратная сортривка
-
 
         //Данные
         $scope.Addresses = null;
@@ -14,9 +85,7 @@
             alert('Error!'); 
         });
 
-
         //Пагинация
-
         $scope.currentPage = 0;
         $scope.itemsPerPage = 10;
         $scope.firstPage = function () {
