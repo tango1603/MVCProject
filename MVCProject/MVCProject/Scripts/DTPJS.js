cf6e33d834341b2daa6f81dee93d2c66d7771805
Ilya Bashilov <tango1603@yandex.ru>
2017-06-24 20:59:02 +0300

    fix bug.

--- MVCProject/MVCProject/Scripts/DTPJS.js
+++
@@ -1,8 +0,0 @@
-﻿angular.module('MyApp', ["ngMaterial", "smDateTimeRangePicker"])
-    .config(function ($mdThemingProvider, pickerProvider) {
-        pickerProvider.setOkLabel('Save');
-        pickerProvider.setCancelLabel('Close');
-        //  Over ride day names by changing here
-        pickerProvider.setDayHeader('single');  //Options 'single','shortName', 'fullName'
-    }
-    );
